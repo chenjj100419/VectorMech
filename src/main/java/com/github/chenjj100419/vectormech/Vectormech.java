02c55f35e5069b52ef8cf8737ed41423480f804f
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2021-07-09 21:11:26 +0800

    Update Vectormech.java

--- src/main/java/com/github/chenjj100419/vectormech/Vectormech.java
+++ src/main/java/com/github/chenjj100419/vectormech/Vectormech.java
@@ -24,6 +24,8 @@ public class Vectormech {
 
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final String MOD_NAME = "VectorMech";
+    private static final String MOD_ID = "vectormech";
 
     public Vectormech() {
         // Register the setup method for modloading
@@ -40,36 +42,21 @@ public class Vectormech {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("vectormech", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
-        });
     }
 
     private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        LOGGER.info(MOD_NAME+" is Loading on start");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
@@ -78,8 +65,6 @@ public class Vectormech {
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
         }
     }
 }
